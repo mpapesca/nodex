e2048b2c5701671125afa0ae4298ad72b473f36b
Michael Papesca <michael.papesca@onmail.com>
2020-10-26 21:51:54 -0400

    Add home stack navigator
    This will add a header to the home screen and allow for future stack navigation.

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import RootDrawer from './src/navigators/RootDrawer';
-
 
 const App = () => {
   return (
@@ -14,12 +12,3 @@ const App = () => {
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/navigators/RootDrawer.js
+++ src/navigators/RootDrawer.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import HomeScreen from '../screens/HomeScreen';
 import InfoScreen from '../screens/InfoScreen';
+import HomeStack from './HomeStack';
 
 const RootDrawer = () => {
 
     const Drawer = createDrawerNavigator();
 
     return (
-        <Drawer.Navigator initialRoute="Home">
-            <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Navigator>
+            <Drawer.Screen name="HomeStack" component={HomeStack} />
             <Drawer.Screen name="Info" component={InfoScreen} />
         </Drawer.Navigator>
     );
